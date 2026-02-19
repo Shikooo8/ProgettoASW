50f1468cf0254228c63fc3fc04a1a9e23be689eb
RitaTi7 <rita.tiani14@gmail.com>
2026-02-19 15:37:39 +0000

    Fix ConnessioniServiceImpl

--- connessioni/src/main/java/asw/bettermusic/connessioni/domain/ConnessioniServiceImpl.java
+++ connessioni/src/main/java/asw/bettermusic/connessioni/domain/ConnessioniServiceImpl.java
@@ -25,8 +25,6 @@ public class ConnessioniServiceImpl implements ConnessioniService {
     private ConnessioniEventPublisher connessioniEventPublisher;
 
     /* Crea una nuova connessione, dati utente, seguito e ruolo. */
-    @Override
-    @Transactional
     public Connessione createConnessione(String utente, String seguito, String ruolo) {
         // verifica unicità-> DA VEDERE
         if (connessioniRepository.findByUtenteAndSeguitoAndRuolo(utente, seguito, ruolo) != null) {
@@ -50,44 +48,36 @@ public class ConnessioniServiceImpl implements ConnessioniService {
     }
 
     /* Trova una connessione, dato l'id. */
-    @Override
     public Connessione getConnessione(Long id) {
         return connessioniRepository.findById(id).orElse(null);
     }
 
     /* Trova una connessione, dati utente, seguito e ruolo. */
-    @Override
     public Connessione getConnessione(String utente, String seguito, String ruolo) {
         return connessioniRepository.findByUtenteAndSeguitoAndRuolo(utente, seguito, ruolo);
     }
 
     /* Trova tutte le connessioni. */
-    @Override
     public Collection<Connessione> getConnessioni() {
         return connessioniRepository.findAll();
     }
 
     /* Trova tutte le connessioni di un utente. */
-    @Override
     public Collection<Connessione> getConnessioniByUtente(String utente) {
         return connessioniRepository.findByUtente(utente);
     }
 
     /* Trova tutte le connessioni con un certo ruolo. */
-    @Override
     public Collection<Connessione> getConnessioniByRuolo(String ruolo) {
         return connessioniRepository.findByRuolo(ruolo);
     }
 
     /* Trova tutte le connessioni di un utente con un certo ruolo. */
-    @Override
     public Collection<Connessione> getConnessioniByUtenteAndRuolo(String utente, String ruolo) {
         return connessioniRepository.findByUtenteAndRuolo(utente, ruolo);
     }
 
     /* Cancella una connessione, dati utente, seguito e ruolo. */
-    @Override
-    @Transactional
     public Connessione deleteConnessione(String utente, String seguito, String ruolo) {
         Connessione connessione = getConnessione(utente, seguito, ruolo);
         if (connessione != null) {
